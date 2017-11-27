67a335d5eba1b22f4ff9709dfc78b7c2ed3298fa
Brendan Boyd <firemound@gmail.com>
2017-11-26 22:03:04 -0800

    adds more swarm workers

--- local-test-cluster.sh
+++ local-test-cluster.sh
@@ -6,9 +6,11 @@ docker-machine ls
 IP=$(docker-machine ip trex1)
 echo "trex1 ip $IP"
 docker-machine ssh trex1 "docker swarm init --advertise-addr $IP"
-JOINTOKEN=$(docker-machine ssh trex1 "docker swarm join-token -q manager")
+JOINTOKEN=$(docker-machine ssh trex1 "docker swarm join-token -q worker")
 echo "join token $JOINTOKEN"
 docker-machine ssh trex2 "docker swarm join --token $JOINTOKEN $IP"
+docker-machine ssh trex3 "docker swarm join --token $JOINTOKEN $IP"
+docker-machine ssh trex4 "docker swarm join --token $JOINTOKEN $IP"
 docker-machine ssh trex1 "docker node ls"
 eval $(docker-machine env trex1)
 docker-machine ls
